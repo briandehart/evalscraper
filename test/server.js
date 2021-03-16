63cd9e005214cfa82f4e3ff5d08c1476311880f0
briandehart <briandehart@gmail.com>
2021-03-16 10:53:08 -0500

    Increment version to 0.1.1

--- test/server.js
+++ test/server.js
@@ -2,28 +2,17 @@ const static = require("node-static");
 const http = require("http");
 const fs = require("fs");
 
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200, { "content-type": "text/html" });
-//   fs.createReadStream("index.html").pipe(res);
-// });
-
-// server.listen(process.env.PORT || 3000);
-
-// console.log("serving index.html");
-
 const fileServer = new static.Server("./test", { cache: 7200 });
 
 http
   .createServer((request, response) => {
     fileServer.serve(request, response, (err, res) => {
       if (err) {
-        // An error has occured
         console.error("> Error serving " + request.url + " - " + err.message);
         response.writeHead(err.status, err.headers);
         response.end();
       } else {
-        // The file was served successfully
-        // Logging here will cause suppress log tests to fail
+        // logging here will cause suppress log tests to fail
         // console.log("> " + request.url + " - " + res.message);
       }
     });
